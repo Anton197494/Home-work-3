8e049835f940ac5a64de6871deae617fb8a1175e
Admin <vishlin.a.k@gmail.com>
2025-04-24 20:07:37 +0300

    master

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int a = 50000;
+        System.out.println("Значение переменной с типом а равно "+ a);
+        byte b = 127;
+        System.out.println("Значение переменной с типом b равно "+ b);
+        short c = 32700;
+        System.out.println("Значение переменной с типом c равно "+ c);
+        long d = 5000;
+        System.out.println("Значение переменной с типом d равно "+ d);
+        float e = 3.75f;
+        System.out.println("Значение переменной с типом e равно "+ e);
+        double l = 4.57d;
+        System.out.println("Значение переменной с типом l равно "+ l);
+
     }
+
+
 }
